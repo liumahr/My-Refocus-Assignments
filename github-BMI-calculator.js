5c4c68e72226ab9c31eb01bb0b89f6e1b0fb437d
Leo Mahr <video.nimo@gmail.com>
2023-01-04 16:11:46 +0800

    First New Assingment: BMI Calculator

---
+++ github-BMI-calculator.js
@@ -0,0 +1,14 @@
+//Create a BMI calculator.
+// Formula: weight (kg) / [height (m)]2
+// The formula for BMI is weight in kilograms divided by height in meters squared. If height has been measured in centimeters, divide by 100 to convert this to meters.
+
+let weight = parseFloat(prompt('Please enter your weight in kilograms (kg)'));
+let height = parseFloat(prompt('Please enter your height in meter(m), example: 1.70'));
+
+function calcBMI() {        
+    let heightSQR = height * height;
+    let BMI = weight / heightSQR;
+    console.log(`Your BMI computation based on your weight and height is ${BMI}`);
+};
+
+calcBMI();
